9eb2fd88fadeee494bd7bf59f8526f20628224eb
odomin2 <odomin2@students.towson.edu>
2016-04-27 21:46:52 -0400

    Task 5 – Mutation Testing
    Commit for task 5.  Edited some of the Junit test to try to kill mutants
    that survived.

---
+++ Task5_CoffeeMaker/src/edu/towson/cis/cosc603/project5/coffeemakerTests/MainFactory.java
@@ -0,0 +1,43 @@
+package edu.towson.cis.cosc603.project5.coffeemakerTests;
+
+import edu.towson.cis.cosc603.project5.coffeemaker.Main;
+
+
+
+/**
+ * The class <code>MainFactory</code> implements static methods that return instances of the class <code>{@link Main}</code>.
+ *
+ * @generatedBy CodePro at 4/26/16 6:38 PM
+ * @author Obed Dominguez
+ * @version $Revision: 1.0 $
+ */
+public class MainFactory
+ {
+	/**
+	 * Prevent creation of instances of this class.
+	 *
+	 * @generatedBy CodePro at 4/26/16 6:38 PM
+	 */
+	private MainFactory() {
+	}
+
+
+	/**
+	 * Create an instance of the class <code>{@link Main}</code>.
+	 *
+	 * @generatedBy CodePro at 4/26/16 6:38 PM
+	 */
+	public static Main createMain() {
+		return new Main();
+	}
+
+
+	/**
+	 * Create an instance of the class <code>{@link Main}</code>.
+	 *
+	 * @generatedBy CodePro at 4/26/16 6:51 PM
+	 */
+	public static Main createMain2() {
+		return new Main();
+	}
+}
